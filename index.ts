c3179a76bd558a72fd95aac7ae479f6e37a2beba
chengxiaopang <827226799@qq.com>
2021-04-06 20:55:55 +0800

    package change

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
-export function add(a:number, b:number) : number {
-    return a + b;
+export const common={
+    checkIn(list:any[],item:any,key:string){
+        let i=0;
+        let isIndex=-1;
+        while (i<list.length){
+            if(list[i][key]===item.key){
+                isIndex=i;
+                return isIn;
+            }
+            i++;
+        }
+        return isIndex
+    },
+    add(a:number, b:number) : number {
+        return a + b;
+    }
+
 }
