686515129b101c4af6be57c847557da6616925b6
chengxiaopang <827226799@qq.com>
2021-04-24 16:57:02 +0800

    indedx change

--- index.ts
+++ index.ts
@@ -1,18 +1,3 @@
-export const common={
-    checkIn(list:any[],item:any,key:string){
-        let i=0;
-        let isIndex=-1;
-        while (i<list.length){
-            if(list[i][key]===item.key){
-                isIndex=i;
-                return isIndex;
-            }
-            i++;
-        }
-        return isIndex
-    },
-    add(a:number, b:number) : number {
-        return a + b;
-    }
-
+export function add(a:number, b:number) : number {
+  return a + b;
 }
