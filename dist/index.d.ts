1ecc67aff158413b517de8bf521be82e7477d33f
chengxiaopang <827226799@qq.com>
2021-04-30 09:42:05 +0800

    update index.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,53 @@
-export * from 'treeTable';
+interface XTreeNode {
+    children?: Array<any>;
+}
+export declare class XMath {
+    static wideTraversal<T extends XTreeNode>(root: T, handler: (node: T) => void, sort: (a: T, b: T) => number): void;
+    static deepTraversal<T extends XTreeNode>(root: T, handler: (node: T) => void, sort: (a: T, b: T) => number): void;
+}
+export interface TreeTableNode extends XTreeNode {
+    id?: string;
+    [propName: string]: any;
+    _node?: {
+        id: string;
+        tnLevel: number;
+        padding: string;
+        collapsed: boolean;
+        show: boolean;
+        parent: TreeTableNode | null;
+    };
+}
+export interface TreeTableOptions<T extends TreeTableNode> {
+    indentUnit?: string;
+    indentNumber?: number;
+    compare(a: T, b: T): number;
+    openModel?: boolean;
+    nodes: Array<T>;
+}
+export declare class TreeTable<T extends TreeTableNode> {
+    protected TID_PREFIX: string;
+    protected TID_SPLITER: string;
+    protected TID_START: number;
+    protected indentUnit: string;
+    protected indentNumber: number;
+    protected options: TreeTableOptions<T> | null;
+    protected tableModel: Array<T>;
+    constructor();
+    getTableModel(): Array<T>;
+    setOptions(options: TreeTableOptions<T>): void;
+    protected refresh(onlyUpdateId?: boolean, openAllModel?: boolean): void;
+    protected cleanModel(): void;
+    protected deleteNode(node: T): void;
+    protected deleteNodeGroup(node: T): void;
+    protected addNode(node: T, parent: T): void;
+    getNodeLocation(node: T): number;
+    protected regenerateTreeTable(root: T, level?: number, onlyUpdateId?: boolean, openModal?: boolean): void;
+    protected setPadding(node: T): void;
+    protected initTreeTableNode(node: T, level?: number): void;
+    showNodes(): T[];
+    collapsNode(node: T): void;
+    uncollapse(node: any): void;
+    protected showTId(a: T, b: T): number;
+    commpareStringArray(a: string[], b: string[]): number;
+}
+export {};
--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,284 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("treeTable"), exports);
+exports.TreeTable = exports.XMath = void 0;
+var XMath = /** @class */ (function () {
+    function XMath() {
+    }
+    XMath.wideTraversal = function (root, handler, sort) {
+        if (root === null || root === undefined) {
+            return;
+        }
+        var queue = [];
+        queue.unshift(root);
+        while (queue.length > 0) {
+            var np = queue.shift();
+            if (handler !== undefined && handler !== null) {
+                if (handler !== undefined && handler !== null) {
+                    handler(np);
+                }
+            }
+            if (np.children === undefined || np.children === null) {
+                continue;
+            }
+            if (sort === null || sort === undefined) {
+                np.children
+                    .forEach(function (n, idx) {
+                    queue.push(n);
+                });
+            }
+            else {
+                np.children
+                    .sort(function (a, b) { return sort(a, b); })
+                    .forEach(function (n, idx) {
+                    queue.push(n);
+                });
+            }
+        }
+    };
+    XMath.deepTraversal = function (root, handler, sort) {
+        if (root === undefined || root === null) {
+            return;
+        }
+        var np;
+        var stack = [];
+        stack.push(root);
+        while (stack.length > 0) {
+            np = stack.pop();
+            if (handler !== null && handler !== undefined) {
+                if (handler !== undefined && handler !== null) {
+                    handler(np);
+                }
+            }
+            if (np.children === undefined || np.children === null) {
+                continue;
+            }
+            if (sort === undefined || sort === null) {
+                np.children
+                    .forEach(function (n) {
+                    stack.push(n);
+                });
+            }
+            else {
+                np.children
+                    .sort(function (a, b) {
+                    return sort(a, b);
+                })
+                    .forEach(function (n) {
+                    stack.push(n);
+                });
+            }
+        }
+    };
+    return XMath;
+}());
+exports.XMath = XMath;
+var TreeTable = /** @class */ (function () {
+    function TreeTable() {
+        this.TID_PREFIX = '#';
+        this.TID_SPLITER = '.';
+        this.TID_START = 2;
+        this.indentUnit = 'rem';
+        this.indentNumber = 2;
+        this.options = null;
+        this.tableModel = new Array();
+        this.TID_START = this.TID_PREFIX.length + this.TID_SPLITER.length;
+    }
+    TreeTable.prototype.getTableModel = function () {
+        return this.tableModel;
+    };
+    TreeTable.prototype.setOptions = function (options) {
+        this.cleanModel();
+        this.options = options;
+        if (this.options.indentUnit !== undefined && this.options.indentUnit !== null) {
+            this.indentUnit = this.options.indentUnit;
+        }
+        if (this.options.indentNumber !== undefined && this.options.indentNumber !== null) {
+            this.indentNumber = this.options.indentNumber;
+        }
+        this.refresh();
+    };
+    TreeTable.prototype.refresh = function (onlyUpdateId, openAllModel) {
+        var _this = this;
+        if (onlyUpdateId === void 0) { onlyUpdateId = false; }
+        if (openAllModel === void 0) { openAllModel = false; }
+        if (this.options === null) {
+            return;
+        }
+        this.options.nodes.forEach(function (node, inx) {
+            _this.initTreeTableNode(node);
+            if (node._node != undefined) {
+                node._node["id"] = _this.TID_PREFIX + _this.TID_SPLITER + inx;
+            }
+            _this.regenerateTreeTable(node);
+        });
+    };
+    TreeTable.prototype.cleanModel = function () {
+        this.tableModel.splice(0, this.tableModel.length);
+    };
+    TreeTable.prototype.deleteNode = function (node) {
+        this.tableModel = this.tableModel.filter(function (item) { return node.id != item.id; });
+    };
+    TreeTable.prototype.deleteNodeGroup = function (node) {
+        var _this = this;
+        for (var n = 0; n < this.tableModel.length; n++) {
+            if (this.tableModel[n].id == node.id) {
+                if (node.children && node.children.length > 0) {
+                    node.children.forEach(function (item, index) {
+                        _this.deleteNode(item);
+                    });
+                }
+            }
+        }
+    };
+    TreeTable.prototype.addNode = function (node, parent) {
+        if (node === null || node === undefined) {
+            return;
+        }
+        if (parent != undefined && parent !== null) {
+            if (parent.children != undefined && parent.children != null) {
+                parent.children.push(node);
+                this.tableModel.splice(this.getNodeLocation(node), 1, node);
+            }
+        }
+        else {
+            this.tableModel.push(node);
+        }
+    };
+    TreeTable.prototype.getNodeLocation = function (node) {
+        var inx = -1;
+        for (var i = 0; i < this.tableModel.length; i++) {
+            if (this.tableModel[i].id == node.id) {
+                inx = i;
+                return inx;
+            }
+        }
+        return inx;
+    };
+    TreeTable.prototype.regenerateTreeTable = function (root, level, onlyUpdateId, openModal) {
+        var _this = this;
+        if (level === void 0) { level = 0; }
+        if (onlyUpdateId === void 0) { onlyUpdateId = false; }
+        if (openModal === void 0) { openModal = false; }
+        var node = root;
+        if (node == undefined) {
+            return;
+        }
+        if (!onlyUpdateId) {
+            this.tableModel.push(node);
+        }
+        if (node.children !== null && node.children !== undefined && node.children.length > 0) {
+            node.children.forEach(function (n, idx) {
+                _this.initTreeTableNode(n);
+                n._node.id = node._node.id + '.' + idx;
+                n._node.collapsed = !openModal;
+                n._node.show = !n._node.collapsed;
+                n._node.tnLevel = n._node.tnLevel + 1 + level;
+                n._node.parent = node;
+                _this.setPadding(n);
+                if (n.children.length > 0) {
+                    _this.regenerateTreeTable(n, n._node.tnLevel);
+                }
+                else {
+                    _this.tableModel.push(n);
+                }
+            });
+        }
+        else {
+            return;
+        }
+    };
+    TreeTable.prototype.setPadding = function (node) {
+        if (node._node === undefined || node._node === null) {
+            return;
+        }
+        node._node.padding = (node._node.tnLevel * this.indentNumber) * 0.5 + this.indentUnit;
+    };
+    TreeTable.prototype.initTreeTableNode = function (node, level) {
+        if (level === void 0) { level = 0; }
+        if (node._node === undefined || node._node === null) {
+            node._node = {
+                id: "",
+                tnLevel: level,
+                padding: "",
+                collapsed: true,
+                show: true,
+                parent: null
+            };
+        }
+    };
+    TreeTable.prototype.showNodes = function () {
+        return this.tableModel.filter(function (n) {
+            return n._node.show;
+        });
+    };
+    TreeTable.prototype.collapsNode = function (node) {
+        if (node._node == undefined) {
+            return;
+        }
+        node._node.collapsed = true;
+        this.tableModel.filter(function (n) {
+            return n._node.id != node._node.id && n._node.id.indexOf(node._node.id) > -1;
+        }).forEach(function (n) {
+            if (n._node != undefined) {
+                n._node.collapsed = true;
+                n._node.show = false;
+            }
+        });
+    };
+    TreeTable.prototype.uncollapse = function (node) {
+        if (node._node == undefined) {
+            return;
+        }
+        node._node.collapsed = false;
+        if (node.children != undefined && node.children.length > 0) {
+            node.children.forEach(function (n, idx) {
+                n._node.show = true;
+            });
+        }
+    };
+    TreeTable.prototype.showTId = function (a, b) {
+        var idA = a._node.id.split(this.TID_SPLITER);
+        var idB = b._node.id.split(this.TID_SPLITER);
+        return this.commpareStringArray(idA, idB);
+    };
+    TreeTable.prototype.commpareStringArray = function (a, b) {
+        var lenA = a.length;
+        var lenB = b.length;
+        if (lenA < lenB) {
+            for (var i = 0; i < lenA; i++) {
+                if (a[i] == b[i]) {
+                    continue;
+                }
+                else {
+                    return Number(a[i]) < Number(b[i]) ? -1 : 1;
+                }
+            }
+            return -1;
+        }
+        else if (lenA > lenB) {
+            for (var i = 0; i < lenB; i++) {
+                if (a[i] == b[i]) {
+                    continue;
+                }
+                else {
+                    return Number(a[i]) < Number(b[i]) ? -1 : 1;
+                }
+                return 1;
+            }
+        }
+        else {
+            for (var i = 0; i < lenB; i++) {
+                if (a[i] == b[i]) {
+                    continue;
+                }
+                else {
+                    return Number(a[i]) < Number(b[i]) ? -1 : 1;
+                }
+                return 0;
+            }
+        }
+        return -1;
+    };
+    return TreeTable;
+}());
+exports.TreeTable = TreeTable;
